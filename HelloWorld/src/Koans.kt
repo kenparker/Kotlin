768ba0f943dc67d2468cf56be5078eb3037249c9
Angelo <magangde@yahoo.de>
2018-01-07 12:14:45 +0100

    more demos

---
+++ HelloWorld/src/Koans.kt
@@ -0,0 +1,3 @@
+fun start(): String = "OK"
+
+fun joinOptions(options: Collection<String>) = options.joinToString ( prefix = "[", postfix="]")
---
+++ HelloWorld/src/KotlinTest.kt
@@ -0,0 +1,18 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+class KotlinTest {
+
+    @Test
+    fun testOK() {
+        assertEquals("OK", start())
+    }
+
+    @Test
+    fun testJoin() {
+        var collectionOfStrings: List<String> = listOf("a","b","c")
+        val joinOptions = joinOptions(collectionOfStrings)
+        assertEquals("[a, b, c]",joinOptions)
+
+    }
+}
